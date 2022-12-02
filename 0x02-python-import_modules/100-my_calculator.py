c6990f60ef4515f3ef91c77388a57e0e0cacd5be
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-12-02 07:14:36 -0800

    corrections

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 from sys import argv
-from calculator_1 import add, sub, mul, div
-if __name__ == "__main__":
-    if len(argv) - 1 != 3:
+from calculator_1 import add, div, mul, sub
+
+
+def main():
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-        if argv[2] == "+":
-            print("{:s} + {:s} = {:d}"
-                    .format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
-        elif argv[2] == "-":
-            print("{:s} - {:s} = {:d}"
-                    .format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
-        elif argv[2] == "*":
-            print("{:s} * {:s} = {:d}"
-                    .format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
-        elif argv[2] == "/":
-            print("{:s} / {:s} = {:d}"
-                    .format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == '+':
+            print("{} {} {} = {}".format(a, argv[2], b, add(a, b)))
+        elif argv[2] == '-':
+            print("{} {} {} = {}".format(a, argv[2], b, sub(a, b)))
+        elif argv[2] == '*':
+            print("{} {} {} = {}".format(a, argv[2], b, mul(a, b)))
+        elif argv[2] == '/':
+            print("{} {} {} = {}".format(a, argv[2], b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
+            if __name__ == "__main__":
+                main()
