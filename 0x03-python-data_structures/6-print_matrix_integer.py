576830820315dacefc6108148a74ea4eb273d067
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-12-02 06:52:46 -0800

    corrections

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print()
-    else:
-        for row in range(len(matrix)):
-            for item in range(len(matrix[row])):
-                if item != len(matrix[row]) - 1:
-                    endspace = ' '
-                else:
-                    endspace = ''
-                    print("{:d}".format(matrix[row][item]), end=endspace)
-                    print()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j < len(matrix[i]) - 1:
+                print("{:d} ".format(matrix[i][j]), end="")
+            else:
+                print("{:d}".format(matrix[i][j]), end="")
+                print()
