c43260acb9624ee9c5956232600a46cd91dfbac3
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-12-02 07:19:08 -0800

    corrections

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    res = 0
-    while index <= argv_count:
-        res += int(sys.argv[index])
-        index += 1
-        print("{:d}".format(res))
+import sys
+
+
+def main(*argv):
+    l = len(sys.argv)
+    sum = 0
+    if l > 1:
+        for args in sys.argv:
+            if args != sys.argv[0]:
+                sum = sum + int(args)
+                print(sum)
+                if __name__ == "__main__":
+                    main()
