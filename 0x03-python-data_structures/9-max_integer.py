751e80ab2be8b16d0e482351fa41cd7a79a07401
Nelisiwe Mkhabela <nelisiwecynthia39@gmail.com>
2022-12-02 05:52:04 -0800

    created 9.main file for testing code

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    biggest = 0
+    biggest = 1
     for index in my_list:
         if index > biggest:
             biggest = index
